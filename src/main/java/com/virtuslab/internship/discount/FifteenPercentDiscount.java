6322daff7d73e4450e96a31543ea714e6c0d3596
hardy6413 <irreftw@gmail.com>
2022-03-25 18:15:00 +0100

    written test for FifteenPercentDiscount

--- src/main/java/com/virtuslab/internship/discount/FifteenPercentDiscount.java
+++ src/main/java/com/virtuslab/internship/discount/FifteenPercentDiscount.java
@@ -1,4 +1,24 @@
 package com.virtuslab.internship.discount;
 
+import com.virtuslab.internship.receipt.Receipt;
+
+import java.math.BigDecimal;
+
 public class FifteenPercentDiscount {
+
+    public static String NAME = "FifteenPercentDiscount";
+
+    public Receipt apply(Receipt receipt) {
+        if (shouldApply(receipt)) {
+            var totalPrice = receipt.totalPrice().multiply(BigDecimal.valueOf(0.9));
+            var discounts = receipt.discounts();
+            discounts.add(NAME);
+            return new Receipt(receipt.entries(), discounts, totalPrice);
+        }
+        return receipt;
+    }
+
+    private boolean shouldApply(Receipt receipt) {
+        return receipt.totalPrice().compareTo(BigDecimal.valueOf(50)) >= 0;
+    }
 }
---
+++ src/test/java/com/virtuslab/internship/discount/FifteenPercentDiscountTest.java
@@ -0,0 +1,38 @@
+package com.virtuslab.internship.discount;
+
+import com.virtuslab.internship.product.ProductDb;
+import com.virtuslab.internship.receipt.Receipt;
+import com.virtuslab.internship.receipt.ReceiptEntry;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FifteenPercentDiscountTest {
+
+    @Test
+    void shouldApply15PercentDiscountWhenCartHasAtLeast3GrainProducts() {
+        // Given
+        var productDb = new ProductDb();
+        var bread = productDb.getProduct("Bread");
+        var cereals = productDb.getProduct("Cereals");
+        List<ReceiptEntry> receiptEntries = new ArrayList<>();
+        receiptEntries.add(new ReceiptEntry(bread, 1));
+        receiptEntries.add(new ReceiptEntry(cereals, 2));
+
+        var receipt = new Receipt(receiptEntries);
+        var discount = new FifteenPercentDiscount();
+        var expectedTotalPrice = cereals.price().multiply(BigDecimal.valueOf(2)).add(bread.price()).
+                multiply(BigDecimal.valueOf(0.85));
+
+        // When
+        var receiptAfterDiscount = discount.apply(receipt);
+
+        // Then
+        assertEquals(expectedTotalPrice, receiptAfterDiscount.totalPrice());
+        assertEquals(1, receiptAfterDiscount.discounts().size());
+    }
+}
